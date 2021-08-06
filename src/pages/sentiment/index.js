3bb91f925cae6df9fbcc6de48a648b513d147333
Asfandyar Malik <asfandyarashraf@hotmail.com>
2021-08-06 09:14:40 +0530

    Code refactoring

--- src/pages/sentiment/index.js
+++ src/pages/sentiment/index.js
@@ -1,27 +1,11 @@
 import sentimentImg from "assets/images/sentiment.png";
 
-import "./style.scss";
-
 const Sentiment = () => {
-
   return (
     <div className="sentiment-page">
-      <div className="body">
-        <div>
-          {/* <p className="text-3xl medium mt-12">
-            Sentiment score for your team
-          </p>
-          <p className="text-xl mb-16">
-            Anonymous Sentiment analysis from the open ended survey questions 
-          </p> */
-          }
-          <div className="images-list">
-            <img src={sentimentImg} alt="Sentiment" />
-          </div>
-        </div>
-      </div>
+      <img src={sentimentImg} className="full-w-img" alt="Sentiment" />
     </div>
-  )
+  );
 };
 
 export default Sentiment;
